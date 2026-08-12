7cc0d3de688dd567bd818f449a399e18357fbb86
Nacho27 <ignacionacho2000x@gmail.com>
2026-08-11 22:20:48 -0400

    Entrega mini Desafio 2

--- main3.java
+++ main3.java
@@ -3,7 +3,36 @@ public class main3 {
         String nombrePersona = "Alex";
         int edadPersonaA = 16;
         int nivelPersona = 12;
-        boolean tienemenbreasia = true;
+        boolean tieneMembresia = true;
+        int modoseleccionado = 1;
+        System.out.println("===Jugador===");
+        System.out.println("Nombre: " + nombrePersona);
+        System.out.println("Edad: " + edadPersonaA);
+        System.out.println("Nivel: " + nivelPersona);
+        System.out.println("Membreasia: " + tieneMembresia);
+        System.out.println("Modo: " + modoseleccionado);
+        System.out.println("===Datos===");
+
+        switch (modoseleccionado){
+            case 1:
+                System.out.println("Modo Historia");
+                break;
+            case 2:
+                System.out.println("Modo Competitivo");
+                break;
+            case 3:
+                System.out.println("Modo Entretenamiento");
+                break;
+
+
+        }
+        if (edadPersonaA >= 15 && nivelPersona >= 10 && tieneMembresia == true) {
+            System.out.println("Acceso al modo: Autorizado");
+        }else{
+            System.out.println("Acceso al modo: Denegado");
+
+        }
+
 
 
     }
